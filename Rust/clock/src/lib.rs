16a7e50663edd28e969f6bc9febec1edbbf2016f
taemath <hirotaka081630@gmail.com>
2024-07-25 22:22:08 +0900

    feat(clock) step1&2

--- Rust/clock/src/lib.rs
+++ Rust/clock/src/lib.rs
@@ -1,12 +1,31 @@
-pub struct Clock;
+use std::fmt;
+
+const DAY: i32 = 60 * 24;
+const HOUR: i32 = 60;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hour: i32,
+    minute: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!()
+        let total_minutes = (DAY + (hours * HOUR + minutes) % DAY) % DAY;
+        let hour = total_minutes / 60;
+        let minute = total_minutes % 60;
+
+        Self { hour, minute }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!()
+        Self::new(self.hour, self.minute + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hour, self.minute)
     }
 }
 
