5c4736c9be2964446f8041e523457543d1c0c4b1
taemath <hirotaka081630@gmail.com>
2024-07-25 22:12:21 +0900

    chore(clock) prepare

---
+++ Rust/clock/src/lib.rs
@@ -0,0 +1,13 @@
+pub struct Clock;
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        todo!()
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        todo!()
+    }
+}
+
+
