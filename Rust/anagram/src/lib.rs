b8b06d71375ad1651593650a235a43e41bcb94cf
taemath <hirotaka081630@gmail.com>
2024-07-26 21:45:24 +0900

    feat(anagram) step1&2

--- Rust/anagram/src/lib.rs
+++ Rust/anagram/src/lib.rs
@@ -1,5 +1,24 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let word_sorted = get_sorted(&word);
+
+    possible_anagrams
+        .iter()
+        .filter(|canditate| {
+            let canditate = canditate.to_lowercase();
+
+            word != canditate && word_sorted == get_sorted(&canditate)
+        })
+        .copied()
+        .collect()
 }
+
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
+
+    word_sorted.sort_unstable();
+    word_sorted
+}
+
