9cf98d124b04f8ab61e7e9ad94890494069a6682
taemath <hirotaka081630@gmail.com>
2024-08-02 22:06:25 +0900

    feat(minsweeper) step3

--- Rust/minesweeper/src/lib.rs
+++ Rust/minesweeper/src/lib.rs
@@ -1,3 +1,14 @@
+static DIRECTIONS: [(usize, usize); 8] = [
+    (!0, !0),
+    (0, !0),
+    (1, !0),
+    (!0, 0),
+    (1, 0),
+    (!0, 1),
+    (0, 1),
+    (1, 1),
+];
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     if minefield.is_empty() || minefield[0].is_empty() {
         return vec!["".to_string(); minefield.len()];
@@ -5,46 +16,25 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
 
     let height = minefield.len();
     let width = minefield[0].len();
-    let v: Vec<Vec<char>> = minefield.into_iter().map(|s| s.chars().collect()).collect();
 
-    let directions = [
-        (!0, !0),
-        (0, !0),
-        (1, !0),
-        (!0, 0),
-        (1, 0),
-        (!0, 1),
-        (0, 1),
-        (1, 1),
-    ];
-
-    let mut board = vec![vec![0_i8; width]; height];
-
-    for i in 0..height {
-        for j in 0..width {
-            if v[i][j] == '*' {
-                board[i][j] = std::i8::MIN;
-
-                for &(dx, dy) in directions.iter() {
-                    let x = j.wrapping_add(dx);
-                    let y = i.wrapping_add(dy);
-
-                    if x < width && y < height {
-                        board[y][x] += 1;
+    (0..height)
+        .map(|y| {
+            (0..width)
+                .map(|x| {
+                    if minefield[y].as_bytes()[x] == b'*' {
+                        '*'
+                    } else {
+                        match DIRECTIONS
+                            .iter()
+                            .map(|&(dx, dy)| (x.wrapping_add(dx), y.wrapping_add(dy)))
+                            .filter(|&(x, y)| x < width && y < height)
+                            .filter(|&(x, y)| minefield[y].as_bytes()[x] == b'*')
+                            .count()
+                        {
+                            0 => ' ',
+                            n => (n as u8 + '0' as u8) as char,
+                        }
                     }
-                }
-            }
-        }
-    }
-
-    board
-        .iter()
-        .map(|line| {
-            line.iter()
-                .map(|&c| match c {
-                    0 => " ".to_string(),
-                    _ if c < 0 => "*".to_string(),
-                    _ => c.to_string(),
                 })
                 .collect()
         })
