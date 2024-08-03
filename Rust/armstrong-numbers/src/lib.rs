a69938d9a28d984270e69ab2780ee925415f756c
taemath <hirotaka081630@gmail.com>
2024-08-04 00:12:04 +0900

    feat(armstrong-numbers) step1

--- Rust/armstrong-numbers/src/lib.rs
+++ Rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let digits: Vec<u32> = num
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    let sum: u64 = digits.iter().fold(0_u64, |sum, num| {
+        sum + (*num as u64).pow(digits.len() as u32)
+    });
+
+    num as u64 == sum
 }
