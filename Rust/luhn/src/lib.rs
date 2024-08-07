69b596fb100f2ad5d847a93b91099cb3d0471e57
taemath <hirotaka081630@gmail.com>
2024-08-07 21:30:31 +0900

    feat(luhn) step3&4

--- Rust/luhn/src/lib.rs
+++ Rust/luhn/src/lib.rs
@@ -3,8 +3,9 @@ pub fn is_valid(code: &str) -> bool {
     code.chars()
         .filter(|c| !c.is_whitespace())
         .rev()
-        .try_fold((0, 0), |(sum, count), v| {
-            v.to_digit(10)
+        .try_fold((0, 0), |(sum, count), value| {
+            value
+                .to_digit(10)
                 .map(|num| if count % 2 == 1 { num * 2 } else { num })
                 .map(|num| if num > 9 { num - 9 } else { num })
                 .map(|num| (sum + num, count + 1))
