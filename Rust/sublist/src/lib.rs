dbefe3fa0a51c44413e445dfcb215816760f6d5b
taemath <hirotaka081630@gmail.com>
2024-08-11 14:57:04 +0900

    feature(sublist) step3&4&5

--- Rust/sublist/src/lib.rs
+++ Rust/sublist/src/lib.rs
@@ -9,16 +9,16 @@ pub enum Comparison {
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
     use Comparison::*;
 
-    let sublist = _first_list.is_empty()
+    let is_sublist = _first_list.is_empty()
         || _second_list
             .windows(_first_list.len())
             .any(|w| w == _first_list);
-    let superlist = _second_list.is_empty()
+    let is_superlist = _second_list.is_empty()
         || _first_list
             .windows(_second_list.len())
             .any(|w| w == _second_list);
 
-    match (sublist, superlist) {
+    match (is_sublist, is_superlist) {
         (true, true) => Equal,
         (true, false) => Sublist,
         (false, true) => Superlist,
