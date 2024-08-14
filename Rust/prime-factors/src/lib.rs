de03b4113258687283705a4763c9c3a51ee13480
taemath <hirotaka081630@gmail.com>
2024-08-14 12:08:14 +0900

    feat(prime-factors) step3

--- Rust/prime-factors/src/lib.rs
+++ Rust/prime-factors/src/lib.rs
@@ -1,15 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = vec![];
     let mut num: u64 = n;
-    let mut candidate = 2;
+    let mut canditate: u64 = 2;
 
     while num > 1 {
-        while num % candidate == 0 {
-            factors.push(candidate);
-            num /= candidate;
+        while num % canditate == 0 {
+            factors.push(canditate);
+            num /= canditate;
         }
 
-        candidate += 1;
+        canditate += 1;
     }
 
     factors
